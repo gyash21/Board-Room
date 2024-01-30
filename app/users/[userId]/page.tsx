87430a4a2ba1e6cb7025edcee36e9ce1207e5929
Yash Gupta <gyash21@gmail.com>
2024-01-31 00:03:56 +0530

    update- setting up basic routes

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,20 @@
+interface UserIdPageProps {
+    params: {
+        userId: string;
+    }
+}
+
+
+
+const Page = ({
+    params,
+}: UserIdPageProps) => {
+    return(
+        <div>
+            User ID: {params.userId}
+        </div>
+    )
+}
+
+
+export default Page;
