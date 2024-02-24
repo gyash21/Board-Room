d329b06d8b617a681119ea9cb723079a1b32e0e0
Yash Gupta <gyash21@gmail.com>
2024-02-24 23:59:04 +0530

    update- adding provider

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { AuthLoading, Authenticated,  } from "convex/react";
