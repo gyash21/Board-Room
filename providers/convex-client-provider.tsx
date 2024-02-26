efd0ba8fa843afd8424857ecdafd6ce9ff6a44bc
Yash Gupta <gyash21@gmail.com>
2024-02-26 23:50:57 +0530

    update- provider variables

--- providers/convex-client-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-
-import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-
-import { AuthLoading, Authenticated,  } from "convex/react";
